8922fd192d8e4ff6b57a1ceb775b37a94e73df11
Cna <cna@Cnas-MacBook-Pro.local>
2025-09-04 01:37:33 +0330

    Update test file for Junit 5 style

--- kafnotif-core/src/test/java/com/kafnotif/AppTest.java
+++ kafnotif-core/src/test/java/com/kafnotif/AppTest.java
@@ -1,38 +1,13 @@
 package com.kafnotif;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    void sampleTest() {
+        // This is a dummy test just to make sure everything works
+        assertTrue(true, "Basic test should always pass");
     }
 }
